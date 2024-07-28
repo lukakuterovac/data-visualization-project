ba47460627db505bd30c582e05b87781f383c4c6
Luka Kuterovac <luka.kuterovac@gmail.com>
2024-07-28 13:50:10 +0200

    Update NameSearch to allow comparison of two names

--- app/src/pages/NameSearch.jsx
+++ app/src/pages/NameSearch.jsx
@@ -7,6 +7,8 @@ const NameSearch = () => {
   const [name, setName] = useState("");
   const [data, setData] = useState({});
   const [selectedName, setSelectedName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [secondData, setSecondData] = useState({});
   const [searchResults, setSearchResults] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -35,8 +37,29 @@ const NameSearch = () => {
   };
 
   const handleNameClick = (name, data) => {
-    setSelectedName(name);
-    setData(data);
+    if (selectedName === "") {
+      setSelectedName(name);
+      setData(data);
+    } else if (secondName === "") {
+      if (selectedName === name) {
+        setSelectedName("");
+        setData({});
+      } else {
+        setSecondName(name);
+        setSecondData(data);
+      }
+    } else if (selectedName === name) {
+      setSelectedName(secondName);
+      setData(secondData);
+      setSecondName("");
+      setSecondData({});
+    } else if (secondName === name) {
+      setSecondName("");
+      setSecondData({});
+    } else {
+      setSecondName(name);
+      setSecondData(data);
+    }
   };
 
   const indexOfLastResult = currentPage * resultsPerPage;
@@ -69,20 +92,49 @@ const NameSearch = () => {
 
   const paginationRange = getPaginationRange();
 
+  const handleDeselect = (name) => {
+    return () => {
+      if (selectedName === name) {
+        setSelectedName(secondName);
+        setData(secondData);
+        setSecondName("");
+        setSecondData({});
+      } else if (secondName === name) {
+        setSecondName("");
+        setSecondData({});
+      }
+    };
+  };
+
   return (
     <div className="flex m-3">
       <div className="w-1/3 flex flex-col items-center">
         <div className="flex items-center w-full">
           <input
             type="text"
-            className="border-2 border-gray-300 rounded-lg p-2 w-full"
+            className="border-2 border-gray-300 rounded-lg p-2 mr-2 flex-grow"
             placeholder="Search for a name"
             value={name}
             onChange={(e) => handleUpdate(e.target.value)}
           />
+          {selectedName && (
+            <button
+              className="mr-2 rounded-lg bg-red-500 text-white p-2 flex-grow min-w-fit"
+              onClick={handleDeselect(selectedName)}
+            >
+              {selectedName}
+            </button>
+          )}
+          {secondName && (
+            <button
+              className="rounded-lg bg-red-500 text-white p-2 flex-grow min-w-fit"
+              onClick={handleDeselect(secondName)}
+            >
+              {secondName}
+            </button>
+          )}
         </div>
         <div className="w-full">
-          {/* Render the current page of search results */}
           {currentResults.map(({ name, data }) => (
             <div key={name} className="w-full">
               <div
@@ -90,7 +142,7 @@ const NameSearch = () => {
                   handleNameClick(name, data);
                 }}
                 className={`cursor-pointer p-2 rounded-lg my-2 w-full text-center ${
-                  selectedName === name
+                  selectedName === name || secondName === name
                     ? "bg-green-500 text-white"
                     : "bg-gray-200"
                 }`}
@@ -100,7 +152,6 @@ const NameSearch = () => {
             </div>
           ))}
         </div>
-        {/* Pagination controls */}
         <div className="flex mt-4 space-x-1 w-full justify-center">
           {currentPage > 1 && (
             <>
@@ -155,7 +206,12 @@ const NameSearch = () => {
         </div>
       </div>
       <div className="w-2/3 flex flex-col items-center">
-        <Info name={selectedName} data={data} />
+        <Info
+          name={selectedName}
+          data={data}
+          secondName={secondName}
+          secondData={secondData}
+        />
       </div>
     </div>
   );
@@ -163,16 +219,14 @@ const NameSearch = () => {
 
 export default NameSearch;
 
-const Info = ({ name, data }) => {
+const Info = ({ name, data, secondName, secondData }) => {
   const [selectedGender, setSelectedGender] = useState("");
 
   useEffect(() => {
-    if (data) {
-      if (data["M"]) {
-        setSelectedGender("M");
-      } else if (data["F"]) {
-        setSelectedGender("F");
-      }
+    if (data && data["M"]) {
+      setSelectedGender("M");
+    } else if (data && data["F"]) {
+      setSelectedGender("F");
     }
   }, [data]);
 
@@ -180,14 +234,45 @@ const Info = ({ name, data }) => {
     return <div className="text-3xl">Please select a name.</div>;
   }
 
-  const maleData = data["M"];
-  const femaleData = data["F"];
+  const maleData = data["M"] ? data["M"].Data : [];
+  const femaleData = data["F"] ? data["F"].Data : [];
+  const secondMaleData = secondData["M"] ? secondData["M"].Data : [];
+  const secondFemaleData = secondData["F"] ? secondData["F"].Data : [];
+
+  const bothHaveMale = maleData.length > 0 && secondMaleData.length > 0;
+  const bothHaveFemale = femaleData.length > 0 && secondFemaleData.length > 0;
+
+  if (secondName && !bothHaveMale && !bothHaveFemale) {
+    return (
+      <div className="text-3xl">
+        {name} and {secondName} do not have any data for the same gender
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center">
-      <div className="text-3xl">{name}</div>
+      <div className="text-3xl">
+        <span
+          className={selectedGender === "F" ? "text-pink-300" : "text-blue-500"}
+        >
+          {name}
+        </span>
+        {secondName && (
+          <>
+            <span className="text-gray-700"> vs </span>
+            <span
+              className={
+                selectedGender === "F" ? "text-pink-600" : "text-blue-800"
+              }
+            >
+              {secondName}
+            </span>
+          </>
+        )}
+      </div>
       <div className="flex gap-3">
-        {maleData && (
+        {!secondName && maleData.length > 0 && (
           <div
             className={`cursor-pointer p-2 rounded-lg my-2 w-[100px] text-center ${
               selectedGender === "M" ? "bg-blue-500 text-white" : "bg-gray-200"
@@ -199,7 +284,31 @@ const Info = ({ name, data }) => {
             Male
           </div>
         )}
-        {femaleData && (
+        {!secondName && femaleData.length > 0 && (
+          <div
+            className={`cursor-pointer p-2 rounded-lg my-2 w-[100px] text-center ${
+              selectedGender === "F" ? "bg-pink-300 text-white" : "bg-gray-200"
+            }`}
+            onClick={() => {
+              setSelectedGender("F");
+            }}
+          >
+            Female
+          </div>
+        )}
+        {secondName && bothHaveMale && (
+          <div
+            className={`cursor-pointer p-2 rounded-lg my-2 w-[100px] text-center ${
+              selectedGender === "M" ? "bg-blue-500 text-white" : "bg-gray-200"
+            }`}
+            onClick={() => {
+              setSelectedGender("M");
+            }}
+          >
+            Male
+          </div>
+        )}
+        {secondName && bothHaveFemale && (
           <div
             className={`cursor-pointer p-2 rounded-lg my-2 w-[100px] text-center ${
               selectedGender === "F" ? "bg-pink-300 text-white" : "bg-gray-200"
@@ -214,19 +323,27 @@ const Info = ({ name, data }) => {
       </div>
       {selectedGender === "M" && (
         <>
-          {maleData && (
-            <>
-              <NameGraph name={name} gender={"M"} />
-            </>
+          {maleData.length > 0 && (
+            <NameGraph
+              name={name}
+              data={maleData}
+              secondName={secondName}
+              secondData={secondMaleData}
+              gender={"M"}
+            />
           )}
         </>
       )}
       {selectedGender === "F" && (
         <>
-          {femaleData && (
-            <>
-              <NameGraph name={name} gender={"F"} />
-            </>
+          {femaleData.length > 0 && (
+            <NameGraph
+              name={name}
+              data={femaleData}
+              secondName={secondName}
+              secondData={secondFemaleData}
+              gender={"F"}
+            />
           )}
         </>
       )}
@@ -234,16 +351,14 @@ const Info = ({ name, data }) => {
   );
 };
 
-const NameGraph = ({ name, gender }) => {
+const NameGraph = ({ name, data, secondName, secondData, gender }) => {
   const svgRef = useRef();
 
   useEffect(() => {
-    if (!name || !gender || !names[name]) return;
+    if (!name && !secondName) return;
 
-    const data = names[name];
-    const dataByGender = data[gender] && data[gender]["Data"];
-
-    if (!dataByGender) return;
+    const primaryData = data || [];
+    const secondaryData = secondData || [];
 
     const svg = d3.select(svgRef.current);
     svg.selectAll("*").remove(); // Clear previous elements
@@ -252,8 +367,28 @@ const NameGraph = ({ name, gender }) => {
     const width = +svg.attr("width") - (margin.left + margin.right + 20);
     const height = +svg.attr("height") - (margin.top + margin.bottom + 20);
 
-    const years = dataByGender.map((d) => d.Year);
-    const counts = dataByGender.map((d) => d.Count);
+    const years = Array.from(
+      new Set([
+        ...primaryData.map((d) => d.Year),
+        ...secondaryData.map((d) => d.Year),
+      ])
+    ).sort();
+
+    const primaryCounts = primaryData.reduce((acc, curr) => {
+      acc[curr.Year] = curr.Count;
+      return acc;
+    }, {});
+
+    const secondaryCounts = secondaryData.reduce((acc, curr) => {
+      acc[curr.Year] = curr.Count;
+      return acc;
+    }, {});
+
+    const combinedCounts = years.map((year) => ({
+      year,
+      primary: primaryCounts[year] || 0,
+      secondary: secondaryCounts[year] || 0,
+    }));
 
     const xScale = d3
       .scaleBand()
@@ -263,7 +398,10 @@ const NameGraph = ({ name, gender }) => {
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(counts)])
+      .domain([
+        0,
+        d3.max(combinedCounts, (d) => Math.max(d.primary, d.secondary)),
+      ])
       .nice()
       .range([height, 0]);
 
@@ -273,24 +411,48 @@ const NameGraph = ({ name, gender }) => {
       .append("g")
       .attr("transform", `translate(${margin.left + 30},${margin.top})`);
 
-    g.selectAll(".bar")
-      .data(dataByGender)
+    g.selectAll(".bar.primary")
+      .data(combinedCounts)
       .enter()
       .append("rect")
-      .attr("class", "bar")
+      .attr("class", "bar primary")
       .attr(
         "x",
-        (d) => xScale(String(d.Year)) + (xScale.bandwidth() - barWidth) / 2
+        (d) => xScale(String(d.year)) + (xScale.bandwidth() - barWidth) / 2
       )
       .attr("y", height)
-      .attr("width", barWidth)
+      .attr("width", barWidth / 2)
       .attr("height", 0)
       .attr("fill", gender === "M" ? "#3b82f6" : "#f9a8d4")
       .transition()
       .duration(750)
       .delay((d, i) => i * 50)
-      .attr("y", (d) => yScale(d.Count))
-      .attr("height", (d) => height - yScale(d.Count));
+      .attr("y", (d) => yScale(d.primary))
+      .attr("height", (d) => height - yScale(d.primary));
+
+    if (secondData.length > 0) {
+      g.selectAll(".bar.secondary")
+        .data(combinedCounts)
+        .enter()
+        .append("rect")
+        .attr("class", "bar secondary")
+        .attr(
+          "x",
+          (d) =>
+            xScale(String(d.year)) +
+            (xScale.bandwidth() - barWidth) / 2 +
+            barWidth / 2
+        )
+        .attr("y", height)
+        .attr("width", barWidth / 2)
+        .attr("height", 0)
+        .attr("fill", gender === "M" ? "#1e40af" : "#db2777")
+        .transition()
+        .duration(750)
+        .delay((d, i) => i * 50)
+        .attr("y", (d) => yScale(d.secondary))
+        .attr("height", (d) => height - yScale(d.secondary));
+    }
 
     // Determine n based on the number of years
     const n = Math.ceil(years.length / 10);
@@ -329,7 +491,7 @@ const NameGraph = ({ name, gender }) => {
       )
       .style("text-anchor", "middle")
       .text("Count");
-  }, [name, gender]);
+  }, [name, secondName, gender, data, secondData]);
 
   return <svg ref={svgRef} width={500} height={500}></svg>;
 };
