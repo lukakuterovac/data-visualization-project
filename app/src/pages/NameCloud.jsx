64c918c9381064cc79756db5e0eb8195a538b7b5
Luka Kuterovac <luka.kuterovac@gmail.com>
2024-08-04 13:55:16 +0200

    Update NameCloud UI and fix playback bug

--- app/src/pages/NameCloud.jsx
+++ app/src/pages/NameCloud.jsx
@@ -1,8 +1,8 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import * as d3 from "d3";
 import cloud from "d3-cloud";
 import names from "../../../dataset/name_counts_per_year.json";
+import { Play, Pause, RotateCcw } from "lucide-react";
 
 const NameCloud = () => {
   return (
@@ -19,70 +19,82 @@ const TimelineWithWordCloud = ({ data }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [wordCloudData, setWordCloudData] = useState([]);
-  const [wordCloudPositions, setWordCloudPositions] = useState([]);
+  const [playbackSpeed, setPlaybackSpeed] = useState(1000);
+  const [pendingWordCloudData, setPendingWordCloudData] = useState([]);
 
   const years = Object.keys(data).map((year) => +year);
+  const firstYear = years[0];
+  const lastYear = years[years.length - 1];
+  const currentYear = years[currentIndex];
 
-  useEffect(() => {
-    const updateWordCloudData = (index) => {
+  const updateWordCloudData = useCallback(
+    (index) => {
       const currentYearData = data[years[index]];
       const wordCloudEntries = Object.entries(currentYearData).map(
         ([name, count]) => ({ text: name, size: count })
       );
-      setWordCloudData(wordCloudEntries);
-    };
+      setPendingWordCloudData(wordCloudEntries);
+    },
+    [data, years]
+  );
 
+  useEffect(() => {
+    let interval;
     if (isPlaying) {
-      const interval = setInterval(() => {
+      interval = setInterval(() => {
         setCurrentIndex((prevIndex) => {
           const newIndex = (prevIndex + 1) % years.length;
           updateWordCloudData(newIndex);
           return newIndex;
         });
-      }, 1000);
-      return () => clearInterval(interval);
-    } else {
-      updateWordCloudData(currentIndex);
+      }, playbackSpeed);
     }
-  }, [isPlaying, currentIndex, data]);
+    return () => clearInterval(interval);
+  }, [isPlaying, playbackSpeed, updateWordCloudData, years.length]);
 
   useEffect(() => {
-    if (wordCloudData.length === 0) return;
+    updateWordCloudData(currentIndex);
+  }, [currentIndex]);
+
+  useEffect(() => {
+    if (pendingWordCloudData.length === 0) return;
 
     const sizeScale = d3
       .scaleLinear()
       .domain([
-        d3.min(wordCloudData, (d) => d.size),
-        d3.max(wordCloudData, (d) => d.size),
+        d3.min(pendingWordCloudData, (d) => d.size),
+        d3.max(pendingWordCloudData, (d) => d.size),
       ])
       .range([10, 100]);
 
     const layout = cloud()
       .size([800, 400])
       .words(
-        wordCloudData.map((d) => ({ text: d.text, size: sizeScale(d.size) }))
+        pendingWordCloudData.map((d) => ({
+          text: d.text,
+          size: sizeScale(d.size),
+        }))
       )
-      .padding(5)
+      .padding(2)
       .rotate(() => ~~(Math.random() * 2) * 90)
       .font("Impact")
       .fontSize((d) => d.size)
       .on("end", (words) => {
-        setWordCloudPositions(words);
-        draw(words);
+        setWordCloudData(words);
       });
 
     layout.start();
-  }, [wordCloudData]);
+  }, [pendingWordCloudData]);
 
   useEffect(() => {
-    if (wordCloudPositions.length > 0) {
-      draw(wordCloudPositions);
+    if (wordCloudData.length > 0) {
+      draw(wordCloudData);
     }
-  }, [wordCloudPositions]);
+  }, [wordCloudData]);
 
   const draw = (words) => {
     const wordCloudSvg = d3.select(wordCloudRef.current);
-    wordCloudSvg.selectAll("*").remove(); // Clear previous elements
+    wordCloudSvg.selectAll("*").remove();
 
     wordCloudSvg
       .attr("width", 800)
@@ -102,22 +114,56 @@ const TimelineWithWordCloud = ({ data }) => {
   };
 
   return (
-    <div>
-      <input
-        type="range"
-        min="0"
-        max={years.length - 1}
-        value={currentIndex}
-        onChange={(e) => setCurrentIndex(+e.target.value)}
-      />
-      <div>
-        <button onClick={() => setIsPlaying(!isPlaying)}>
-          {isPlaying ? "Pause" : "Play"}
-        </button>
-        <button onClick={() => setCurrentIndex(0)}>Reset</button>
+    <div className="m-3">
+      <div className="flex">
+        <div className="p-0 m-2">{firstYear}</div>
+        <input
+          type="range"
+          min="0"
+          max={years.length - 1}
+          value={currentIndex}
+          onChange={(e) => setCurrentIndex(+e.target.value)}
+          className="w-full"
+        />
+        <div className="p-0 m-2">{lastYear}</div>
       </div>
-      <svg ref={wordCloudRef}></svg>
-      <div>Year: {years[currentIndex]}</div>
+      <div className="flex gap-3 w-full justify-center items-center mb-6">
+        <div>Current year: {currentYear}</div>
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? (
+            <div className="flex items-center gap-1 bg-yellow-500 py-1 px-2 rounded-md">
+              <Pause size={14} />
+              Pause
+            </div>
+          ) : (
+            <div className="flex items-center gap-1 bg-green-500 py-1 px-2 rounded-md text-white">
+              <Play size={14} />
+              Play
+            </div>
+          )}
+        </button>
+        <button
+          className="flex items-center gap-1 bg-red-500 py-1 px-2 rounded-md text-white"
+          onClick={() => setCurrentIndex(0)}
+        >
+          <RotateCcw size={14} />
+          Reset
+        </button>
+        <select
+          value={playbackSpeed}
+          onChange={(e) => setPlaybackSpeed(+e.target.value)}
+          className="border-2 rounded-md py-1 px-2"
+        >
+          <option value="2000">0.5x</option>
+          <option value="1000">1x</option>
+          <option value="500">2x</option>
+          <option value="250">4x</option>
+          <option value="125">8x</option>
+        </select>
+      </div>
+      <div className="w-full flex justify-center">
+        <svg ref={wordCloudRef}></svg>
+      </div>
     </div>
   );
 };
